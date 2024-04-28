2a03bb8e0588ea4a9876d46a357cc80a09ac2895
Anastasia Tikhonova <honu347@gmail.com>
2024-04-28 21:41:43 +0200

    Added api routes

---
+++ photo-feed/src/app/api/[tags]/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { tags: string } }
+) {
+  const tags = params.tags;
+  const res = await fetch(
+    `https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1&tags=${tags}`
+  );
+  const data = await res.json();
+
+  return NextResponse.json({ data });
+}
---
+++ photo-feed/src/app/api/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const res = await fetch(
+    "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1"
+  );
+  const data = await res.json();
+
+  return NextResponse.json({ data });
+}
