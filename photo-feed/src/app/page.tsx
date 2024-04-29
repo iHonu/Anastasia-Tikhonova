f0c8fd3ba95c2b67d4c2a24017ee34394b44b8ed
Anastasia Tikhonova <honu347@gmail.com>
2024-04-29 17:26:51 +0200

    Refactor search param handling to avoid SSR issues

--- photo-feed/src/app/page.tsx
+++ photo-feed/src/app/page.tsx
@@ -1,9 +1,13 @@
 import Search from "@/components/Search";
+import { Suspense } from 'react'
 
 export default function Home() {
   return (
     <main>
+      <Suspense>
       <Search />
+      </Suspense>
+      
     </main>
   );
 }
