e57cdafa5fa9660df42e469211aa6220c2e3e62c
Anastasia Tikhonova <honu347@gmail.com>
2024-04-29 09:41:25 +0200

    Refactor search functionality to use URL parameters and debouncing

--- photo-feed/src/components/Search.tsx
+++ photo-feed/src/components/Search.tsx
@@ -1,49 +1,67 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { useState, useEffect } from "react";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import PhotoCard from "@/components/PhotoCard";
-import { useState, useEffect } from "react";
+import { useDebounce } from "use-debounce";
 
 export default function Search() {
-  const [text, setText] = useState("");
   const [loading, setLoading] = useState(false);
   const [feed, setFeed] = useState<PhotosProps[]>([]);
 
-  const fetchNewData = async () => {
-    setLoading(true);
-    try {
-      const res = await fetch(`/api/${text}`);
-      if (!res.ok) {
-        throw new Error("Failed to fetch data");
-      }
-      const data = await res.json();
-      setFeed(data.data.items);
-      console.log(data);
-    } catch (error) {
-      console.error(error);
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = (term: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
     }
-    setLoading(false);
+    replace(`${pathname}?${params.toString()}`);
   };
 
+  const query = searchParams.get("query") || "";
+  const [debouncedQuery] = useDebounce(query, 550);
+
   useEffect(() => {
+    const fetchNewData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(`/api/${debouncedQuery}`);
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await res.json();
+        setFeed(data.data.items);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     fetchNewData();
-  }, []);
+  }, [debouncedQuery]);
 
   return (
     <div className="flex flex-col mx-auto gap-36 items-center justify-center ">
       <div className="flex w-full max-w-sm items-center justify-center space-x-2 mt-12 ml-12">
         <Input
-          onChange={(e) => setText(e.target.value)}
+          onChange={(e) => {
+            handleSearch(e.target.value);
+          }}
           type="text"
           placeholder="Search Tags"
+          className="px-4 py-2 border rounded"
         />
-        <Button onClick={fetchNewData} disabled={loading} type="submit">
-          Search
-        </Button>
       </div>
       <div className="flex justify-center gap-4 flex-wrap max-w-[60vw]">
-        {feed.map((item: PhotosProps) => (
+        {feed.map((item) => (
           <PhotoCard
             key={item.date_taken}
             title={item.title}
